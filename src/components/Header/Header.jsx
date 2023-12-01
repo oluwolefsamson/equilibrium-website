584bb07675b619b010021f8d378e4e08ea4a6f1f
Oluwole samson <oluwolefsamson44@gmail.com>
2023-12-01 11:53:50 -0800

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
   return (
     <section className='h-wrapper'>
     <div className='flexCenter paddings innerWidth h-container'>
-        <img src='./logo.png' alt='logo' width={100} />
+        <img src='./logo.png' alt='logo' />
 
         <OutsideClickHandler
         onOutsideClick={()=> {
